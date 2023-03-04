719cd409867558f18dc78050994ad58041491397
Muhammad Abbas <abbas@vigoroustechnologies.com>
2023-03-03 22:37:52 -0600

    Initial Commit

---
+++ CustomTabbarSwiftUI/CustomTabbarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabbarSwiftUIApp.swift
+//  CustomTabbarSwiftUI
+//
+//  Created by Zeeshan Suleman on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabbarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
